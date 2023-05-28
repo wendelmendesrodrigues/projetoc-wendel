023affda46e66b1e3df330cdc26d39610daf6e36
Wendel Mendes Rodrigues <wendel.rodrigues@etec.sp.gov.br>
2023-05-28 11:45:15 -0300

    Alteração no código

--- main.c
+++ main.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 main() {
+	
+int numero1;
+int numero2;
 
-printf("Boa Tarde!\n");
-printf("Boa Noite!");
-
+numero1 = 59;
+numero2 = 62;
+printf("Digite um valor para o número 1: ");
+scanf("%d",&numero1);
+printf("Digite um valor para o número 2: ");
+scanf("%d",&numero2);
+printf("O numero 1: %d e o numero 2: %d",numero1, numero2);
 }
